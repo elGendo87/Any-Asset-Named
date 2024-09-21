91009b2ee4d87a8a37404de60a6aaa0b0a1fda5e
Fede <Fede@FEDE-PC>
2024-09-21 16:04:10 -0300

    Agregar archivos de proyecto.

---
+++ Mod.cs
@@ -0,0 +1,26 @@
+﻿using Colossal.Logging;
+using Game;
+using Game.Modding;
+using Game.SceneFlow;
+
+namespace Any_Asset_Named
+{
+    public class Mod : IMod
+    {
+        //public static ILog log = LogManager.GetLogger($"{nameof(Any_Asset_Named)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
+
+        public void OnLoad(UpdateSystem updateSystem)
+        {
+            //log.Info(nameof(OnLoad));
+
+           // if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
+                //log.Info($"Current mod asset at {asset.path}");
+
+        }
+
+        public void OnDispose()
+        {
+            //log.Info(nameof(OnDispose));
+        }
+    }
+}
